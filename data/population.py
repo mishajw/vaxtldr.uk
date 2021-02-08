d9a87814553d3427e6b8cb999ef7086fe809384b
Misha Wagner <mishajw@gmail.com>
2021-02-08 18:43:28 +0000

    Use England population instead of UK population.
    Forgot to update this when moving the site from UK to England.
    
    Fixes #2.

--- data/population.py
+++ data/population.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 # Source: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationprojections/datasets/tablea21principalprojectionukpopulationinagegroups
+UK_OVER_80_FRACTION = 3497000 / (3497000 + 64094000)
+# Source: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/bulletins/annualmidyearpopulationestimates/mid2019estimates#population-growth-in-england-wales-scotland-and-northern-ireland
+ENGLAND_POPULATION = 56286961
+
 __POPULATION_BY_GROUP = {
-    "<80": 64094000,
-    ">=80": 3497000,
-    "all": 64094000 + 3497000,
+    "<80": ENGLAND_POPULATION * UK_OVER_80_FRACTION,
+    ">=80": ENGLAND_POPULATION * (1 - UK_OVER_80_FRACTION),
+    "all": ENGLAND_POPULATION,
 }
 
 
