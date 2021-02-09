58f69493204d72a5fbc0a313379b884eee81dd0d
Misha Wagner <mishajw@gmail.com>
2021-02-09 16:34:13 +0000

    Fix over 80s population bug.

--- data/population.py
+++ data/population.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Source: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationprojections/datasets/tablea21principalprojectionukpopulationinagegroups
-UK_OVER_80_FRACTION = 3497000 / (3497000 + 64094000)
+__UK_OVER_80_FRACTION = 3497000 / (3497000 + 64094000)
 # Source: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/bulletins/annualmidyearpopulationestimates/mid2019estimates#population-growth-in-england-wales-scotland-and-northern-ireland
-ENGLAND_POPULATION = 56286961
+__ENGLAND_POPULATION = 56286961
 
 __POPULATION_BY_GROUP = {
-    "<80": ENGLAND_POPULATION * UK_OVER_80_FRACTION,
-    ">=80": ENGLAND_POPULATION * (1 - UK_OVER_80_FRACTION),
-    "all": ENGLAND_POPULATION,
+    "<80": int(__ENGLAND_POPULATION * (1 - __UK_OVER_80_FRACTION)),
+    ">=80": int(__ENGLAND_POPULATION * __UK_OVER_80_FRACTION),
+    "all": __ENGLAND_POPULATION,
 }
 
 
