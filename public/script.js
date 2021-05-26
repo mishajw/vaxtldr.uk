3235b88b8ea4782880d5a9179d87a4e251a16dac
Chris Lane <chris@chrislane.com>
2021-05-26 10:34:59 +0100

    Modernise some JS
    - Use const wherever possible
    - Use arrow functions since they're a bit cleaner looking
    - Use `.then` instead of `await`. await is cleaner in some cases but using
    it everywhere seems a bit silly, making everything synchronous :P
    
    Side note: csv not being immutable, needing to load the file twice due
    to filtering/mapping operations is annoying #ImmutableByDefault

--- public/script.js
+++ public/script.js
@@ -19,77 +19,74 @@ const GROUPS = ["<=39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "7
 const GOVERNMENT_TARGET_NUM = 44_000_000;
 const GOVERNMENT_TARGET_PERCENT = GOVERNMENT_TARGET_NUM / 56_286_961;
 
-async function start() {
-    let latestDataDate = await getLatestDataDate();
-    await initializeBarCharts();
-    await initializeLineCharts(latestDataDate);
+function start() {
+    getLatestDataDate().then(initializeLineCharts);
+    initializeBarCharts();
 }
 
-async function initializeBarCharts() {
-    let csv = await d3.csv("latest.csv");
-    makeBarChart(
-        "bar-all",
-        "Percent of England vaccinated",
-        csv.filter(function(row) {
-            return row.group == "all";
-        }),
-        [
-            herdImmunityAnnotation("vertical", "x", true),
-            governmentTargetAnnotation("vertical", "x", true),
-        ],
-        false /* showGroups */);
-    makeBarChart(
-        "bar-over-80",
-        "Percent of >80s" + SUPERSCRIPT_4 + " vaccinated",
-        csv,
-        [],
-        true /* showGroups */);
+function initializeBarCharts() {
+    return d3.csv("latest.csv")
+        .then(csv => {
+            makeBarChart(
+                "bar-all",
+                "Percent of England vaccinated",
+                csv.filter(row => row.group === "all"),
+                [
+                    herdImmunityAnnotation("vertical", "x", true),
+                    governmentTargetAnnotation("vertical", "x", true)
+                ],
+                false /* showGroups */);
+            makeBarChart(
+                "bar-over-80",
+                "Percent of >80s" + SUPERSCRIPT_4 + " vaccinated",
+                csv,
+                [],
+                true /* showGroups */);
+        });
 }
 
 function makeBarChart(id, title, csv, annotations, showGroups) {
-    let vaccinated_per_dose = DOSES.map(function(dose) {
+    const vaccinated_per_dose = DOSES.map(dose => {
         // TODO: Unnest function.
-        let vaccinated = csv
-            .filter(function(row) {
+        const vaccinated = csv
+            .filter(row => {
                 if (showGroups && GROUPS.indexOf(row.group) === -1) {
                     return false;
                 }
                 if (!showGroups && row.group !== "all") {
                     return false;
                 }
-                return row.dose == dose;
+                return row.dose === dose;
             })
-            .map(function(row) {
-                let vaccinated = parseInt(row.vaccinated);
-                let population = parseFloat(row.population);
+            .map(row => {
+                const vaccinated = parseInt(row.vaccinated);
+                const population = parseFloat(row.population);
                 return {
                     x: (vaccinated / population) * 100,
                     vaccinated: vaccinated,
                     population: population,
-                    group: row.group,
+                    group: row.group
                 };
             })
-            .sort(function(v1, v2) {
-                return GROUPS.indexOf(v1.group) - GROUPS.indexOf(v2.group);
-            });
+            .sort((v1, v2) => GROUPS.indexOf(v1.group) - GROUPS.indexOf(v2.group));
         return {
             label: DOSE_LABELS[dose],
             backgroundColor: DOSE_COLORS[dose],
-            data: vaccinated,
+            data: vaccinated
         };
     });
     new Chart(id, {
         type: "horizontalBar",
         data: {
             labels: showGroups ? GROUPS : [title],
-            datasets: vaccinated_per_dose,
+            datasets: vaccinated_per_dose
         },
         options: {
             tooltips: {
                 callbacks: {
-                    label: function(tooltipItem, data) {
-                        let dataset = data.datasets[tooltipItem.datasetIndex]
-                        let datasetData = dataset.data[tooltipItem.index];
+                    label: (tooltipItem, data) => {
+                        const dataset = data.datasets[tooltipItem.datasetIndex];
+                        const datasetData = dataset.data[tooltipItem.index];
                         return dataset.label + ": " +
                             datasetData.x.toFixed(2) + "%" +
                             " (" + formatPopulation(datasetData.vaccinated) +
@@ -101,121 +98,109 @@ function makeBarChart(id, title, csv, annotations, showGroups) {
                 yAxes: [{
                     stacked: true,
                     ticks: {
-                        display: showGroups,
+                        display: showGroups
                     }
                 }],
                 xAxes: [{
-                    id: 'x',
+                    id: "x",
                     stacked: false,
                     ticks: {
                         min: 0,
                         max: 100,
-                        callback: function(value) {
-                            return value + "%"
-                        }
+                        callback: value => value + "%"
                     }
-                }],
+                }]
             },
             annotation: {
                 drawTime: "afterDatasetsDraw",
-                annotations: annotations,
-            },
+                annotations: annotations
+            }
         }
     });
 }
 
-async function initializeLineCharts(latestDataDate) {
-    let csv = await d3.csv("line.csv");
-    let herdImmunityDate = '';
-    csv.forEach(function(row) {
-        if (herdImmunityDate != '' || row.dose != "2_wait") {
-            return;
-        }
-        let vaccinated = parseInt(row.vaccinated);
-        let population = parseFloat(row.population);
-        if ((vaccinated / population) > 0.7) {
-            herdImmunityDate = row.real_date;
-        }
-    });
+function initializeLineCharts(latestDataDate) {
+    return d3.csv("line.csv")
+        .then(csv => {
+            const herdImmunityDate = csv.find(row => {
+                const vaccinated = parseInt(row.vaccinated);
+                const population = parseFloat(row.population);
+                return row.dose !== "2_wait" && (vaccinated / population) > 0.7;
+            }).real_date;
 
-    let csvNotExtrapolated = csv.filter(function(row) {
-        return row.extrapolated == "False";
-    });
-    makeLineChart(
-        "line",
-        csvNotExtrapolated,
-        [
-            governmentTargetAnnotation("horizontal", "y", false),
-            {
-                mode: "vertical",
-                scaleID: "x",
-                type: "line",
-                display: true,
-                value: '2021-07-31',
-                borderColor: "#D5212E",
-                borderWidth: 2,
-            }
-        ],
-        true);
-    makeLineChart(
-        "line-extrapolated",
-        csv,
-        [
-            herdImmunityAnnotation("horizontal", "y", false),
-            {
-                mode: "vertical",
-                scaleID: "x",
-                type: "line",
-                display: true,
-                value: herdImmunityDate,
-                borderColor: "#FFD700",
-                borderWidth: 2,
-                label: {
-                    content: 'Est. ' + new Date(herdImmunityDate).toLocaleDateString(),
-                    enabled: true,
-                }
-            },
-            {
-                mode: "vertical",
-                scaleID: "x",
-                type: "line",
-                display: true,
-                value: latestDataDate,
-                borderColor: "#03a5fc",
-                borderWidth: 2,
-                label: {
-                    content: 'Latest data, ' + new Date(latestDataDate).toLocaleDateString(),
-                    enabled: true,
-                }
-            }
-        ]);
+            const csvNotExtrapolated = csv.filter(row => row.extrapolated === "False");
+            makeLineChart(
+                "line",
+                csvNotExtrapolated,
+                [
+                    governmentTargetAnnotation("horizontal", "y", false),
+                    {
+                        mode: "vertical",
+                        scaleID: "x",
+                        type: "line",
+                        display: true,
+                        value: "2021-07-31",
+                        borderColor: "#D5212E",
+                        borderWidth: 2
+                    }
+                ],
+                true);
+            makeLineChart(
+                "line-extrapolated",
+                csv,
+                [
+                    herdImmunityAnnotation("horizontal", "y", false),
+                    {
+                        mode: "vertical",
+                        scaleID: "x",
+                        type: "line",
+                        display: true,
+                        value: herdImmunityDate,
+                        borderColor: "#FFD700",
+                        borderWidth: 2,
+                        label: {
+                            content: "Est. " + new Date(herdImmunityDate).toLocaleDateString(),
+                            enabled: true
+                        }
+                    },
+                    {
+                        mode: "vertical",
+                        scaleID: "x",
+                        type: "line",
+                        display: true,
+                        value: latestDataDate,
+                        borderColor: "#03a5fc",
+                        borderWidth: 2,
+                        label: {
+                            content: "Latest data, " + new Date(latestDataDate).toLocaleDateString(),
+                            enabled: true
+                        }
+                    }
+                ]);
+        });
 }
 
 function makeLineChart(id, csv, annotations, limit) {
-    let dates = csv
-        .map(function(row) {
-            return row.real_date;
-        })
+    const dates = csv
+        .map(row => row.real_date)
         .filter(distinct);
-    let datasets = DOSES.map(function(dose) {
-        let vaccinated = csv
-            .filter(function(row) {
-                return row.dose == dose;
-            })
-            .map(function(row) {
-                let vaccinated = parseInt(row.vaccinated);
-                let population = parseFloat(row.population);
+    const datasets = DOSES.map(dose => {
+        const vaccinated = csv
+            .filter(row => row.dose === dose)
+            .map(row => {
+                const vaccinated = parseInt(row.vaccinated);
+                const population = parseFloat(row.population);
                 return {
                     x: row.real_date,
                     y: (vaccinated / population) * 100,
                     vaccinated: vaccinated,
-                    population: population,
+                    population: population
                 };
             });
         return {
             label: DOSE_LABELS[dose],
             backgroundColor: DOSE_COLORS[dose],
-            data: vaccinated,
+            data: vaccinated
         };
     });
 
@@ -223,14 +208,14 @@ function makeLineChart(id, csv, annotations, limit) {
         type: "line",
         data: {
             labels: dates,
-            datasets: datasets,
+            datasets: datasets
         },
         options: {
             tooltips: {
                 callbacks: {
-                    label: function(tooltipItem, data) {
-                        let dataset = data.datasets[tooltipItem.datasetIndex]
-                        let datasetData = dataset.data[tooltipItem.index];
+                    label: (tooltipItem, data) => {
+                        const dataset = data.datasets[tooltipItem.datasetIndex];
+                        const datasetData = dataset.data[tooltipItem.index];
                         return dataset.label + ": " +
                             datasetData.y.toFixed(2) + "%" +
                             " (" + formatPopulation(datasetData.vaccinated) +
@@ -243,40 +228,40 @@ function makeLineChart(id, csv, annotations, limit) {
                     id: "x",
                     type: "time",
                     time: {
-                        max: limit ? '2021-08-10' : undefined,
+                        max: limit ? "2021-08-10" : undefined
                     }
                 }],
                 yAxes: [{
                     id: "y",
                     ticks: {
                         min: 0,
-                        callback: function(value) {
-                            return value + "%"
-                        }
+                        callback: value => value + "%"
                     }
-                }],
+                }]
             },
             annotation: {
                 drawTime: "afterDatasetsDraw",
-                annotations: annotations,
-            },
-        },
+                annotations: annotations
+            }
+        }
     });
 }
 
 /**
  * @returns Promise<Date>
  */
-async function getLatestDataDate() {
-    let response = await fetch("freshness.txt");
-    let text = await response.text();
-    let split = text.split(" ");
-    let runDate = new Date(split[0]);
-    let dataDate = new Date(split[1]);
-    document.getElementById("freshness").innerHTML =
-        "Last updated on " + runDate.toLocaleDateString('en-GB') + ", " +
-        " with data from " + dataDate.toLocaleDateString('en-GB') + ".";
-    return dataDate;
+function getLatestDataDate() {
+    return fetch("freshness.txt")
+        .then(response => response.text())
+        .then(text => {
+            const [runDate, dataDate] = text
+                .split(" ")
+                .map(dateStr => new Date(dateStr));
+            document.getElementById("freshness").innerHTML =
+                "Last updated on " + runDate.toLocaleDateString("en-GB") + ", " +
+                " with data from " + dataDate.toLocaleDateString("en-GB") + ".";
+            return dataDate;
+        });
 }
 
 function distinct(value, index, self) {
@@ -301,13 +286,13 @@ function herdImmunityAnnotation(mode, scaleId, adjust) {
         scaleID: scaleId,
         type: "line",
         display: true,
-        value: '70',
+        value: "70",
         borderColor: "#FFD700",
         borderWidth: 2,
         label: {
             content: "Herd immunity" + SUPERSCRIPT_3 + " (dose 2 +7d)",
             enabled: true,
-            yAdjust: adjust ? -20 : 0,
+            yAdjust: adjust ? -20 : 0
         }
     };
 }
@@ -324,7 +309,7 @@ function governmentTargetAnnotation(mode, scaleId, adjust) {
         label: {
             content: "July target" + SUPERSCRIPT_2 + " (dose 1)",
             enabled: true,
-            yAdjust: adjust ? 20 : 0,
+            yAdjust: adjust ? 20 : 0
         }
     };
 }
